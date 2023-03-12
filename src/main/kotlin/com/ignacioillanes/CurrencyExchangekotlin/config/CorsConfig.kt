86f2078042c1bb0d572165af7442f0d1324fd267
iguiIllanes <igui10082002@gmail.com>
2023-03-11 22:04:47 -0400

    Add CORS configuration file

---
+++ src/main/kotlin/com/ignacioillanes/CurrencyExchangekotlin/config/CorsConfig.kt
@@ -0,0 +1,18 @@
+package com.ignacioillanes.CurrencyExchangekotlin.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsFilter() = org.springframework.web.filter.CorsFilter(org.springframework.web.cors.UrlBasedCorsConfigurationSource().apply {
+        registerCorsConfiguration("/**", org.springframework.web.cors.CorsConfiguration().apply {
+            allowedOrigins = listOf("http://localhost:4200")
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+            exposedHeaders = listOf("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+            maxAge = 3600
+        })
+    })
+}
